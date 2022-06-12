27df9503f16427de0644662e842e80c21f43cc94
Lorene Pecci <lorenepecci@gmail.com>
2022-06-12 16:12:32 -0300

    controller done

--- backEnd/src/controllers/patientController.js
+++ backEnd/src/controllers/patientController.js
@@ -1,7 +1,33 @@
 const express = require( 'express' );
-const route = express.Router();
+const router = express.Router();
+const service = require('../services/patientService');
 
+router.post('/', async (req, res) => {
+    const response = await service.createPatient(req.body);
+    return res.status(201).json(response);
+}); 
 
+router.get('/', async (_req, res) => {
+    const patient = await service.getPatient();
+    return res.status(201).json(patient);
+});
 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const isUpdate = await service.updatePatient(id, req.body);
+    if (isUpdate) {
+        return res.status(201).json({ message: `Registro ${id} atualizado com sucesso!` });
+    }
+    res.status(404).json({ message: `Erro ao remover o registro ${id}` });
+});
 
-module.exports = route
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const isRemoved = await service.removePatient(id);
+    if (isRemoved) {
+        return res.status(201).json({ message: `Registro ${id} removido com sucesso!` });
+    }
+    res.status(404).json({ message: `Erro ao remover o registro ${id}` });
+});
+
+module.exports = router
