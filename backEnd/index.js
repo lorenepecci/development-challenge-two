5f90f6883bd39f3609428f065be120b0ef667a29
Lorene Pecci <lorenepecci@gmail.com>
2022-06-12 15:57:29 -0300

    rest api constructed

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,7 +2,7 @@ require( 'dotenv' ).config();
 const express = require( 'express' );
 app = express();
 app.use( express.json() );
-
+app.use( routes );
 const port = process.env.API_PORT || 3000;
 
 app.listen(port, () => console.log('ouvindo porta', port));
---
+++ backEnd/src/routes.js
@@ -0,0 +1,6 @@
+const express = require( 'express' );
+const route = express.Router();
+const patientController = require( './controllers/patientController' );
+
+route.use( '/patient', patientController );
+module.exports = route
